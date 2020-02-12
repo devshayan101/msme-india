de44b43be1fd6ae9634c692436e9e149c5615734
Dev <shayan.dev98@gmail.com>
2020-02-13 04:53:02 +0530

    multer-error-fix-undefined

--- app.js
+++ app.js
@@ -70,7 +70,9 @@ app.get('/disclaimer', (req, res) => {
 })
 
 app.post('/submit', upload, (req, res, err) => {
-  const adminOutput = `
+
+  if (!err) {
+    const adminOutput = `
     <p>You have a new Registration</p>
     <h3>Details</h3>
     <ul>
@@ -94,7 +96,7 @@ app.post('/submit', upload, (req, res, err) => {
     </ul>
   
   `;
-  const userOutput = `
+    const userOutput = `
     <p>Your form have been submitted successfully.</p>
     <p>Following are the details submitted by you.</p> 
     <h3>Details</h3>
@@ -119,92 +121,228 @@ app.post('/submit', upload, (req, res, err) => {
     </ul>
   `;
 
-  var imgName01 = req.files['aadhaarFile01'][0].filename;
-  var imgNameString01 = imgName01.toString();
-  var imgPath01 = req.files['aadhaarFile01'][0].path;
-  var imgPathString01 = imgPath01.toString();
 
-  var imgName02 = req.files['aadhaarFile02'][0].filename;
-  var imgNameString02 = imgName02.toString();
-  var imgPath02 = req.files['aadhaarFile02'][0].path;
-  var imgPathString02 = imgPath02.toString();
-
-  var imgName03 = req.files['panFile'][0].filename;
-  var imgNameString03 = imgName03.toString();
-  var imgPath03 = req.files['panFile'][0].path;
-  var imgPathString03 = imgPath03.toString();
+    var imgName01 = req.files['aadhaarFile01'][0].filename;
+    var imgNameString01 = imgName01.toString();
+    var imgPath01 = req.files['aadhaarFile01'][0].path;
+    var imgPathString01 = imgPath01.toString();
 
 
-  // async..await is not allowed in global scope, must use a wrapper
-  async function main() {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    //let testAccount = await nodemailer.createTestAccount(); 
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: process.env.USER, // generated ethereal user
-        pass: process.env.PASS // generated ethereal password
-      }
+    var imgName02 = req.files['aadhaarFile02'][0].filename;
+    var imgNameString02 = imgName02.toString();
+    var imgPath02 = req.files['aadhaarFile02'][0].path;
+    var imgPathString02 = imgPath02.toString();
 
-    });
+    var imgName03 = req.files['panFile'][0].filename;
+    var imgNameString03 = imgName03.toString();
+    var imgPath03 = req.files['panFile'][0].path;
+    var imgPathString03 = imgPath03.toString();
 
-    // send mail with defined transport object
-    //This is Admin Mailing.
-    let infoAdmin = await transporter.sendMail({
-      from: '"MSME Registration" <foo@example.com>', // sender address
-      to: 'eudyogindiaonline@gmail.com', // list of receivers
-      subject: 'ADMIN -MSME Registration', // Subject line
-      //text: 'Hello world?', // plain text body
-      html: adminOutput, // html body
-      attachments: [
-        {
-          filename: imgNameString01,
-          path: imgPathString01
-        },
-        {
-          filename: imgNameString02,
-          path: imgPathString02
-        },
-        {
-          filename: imgNameString03,
-          path: imgPathString03
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+      // Generate test SMTP service account from ethereal.email
+      // Only needed if you don't have a real mail account for testing
+      //let testAccount = await nodemailer.createTestAccount(); 
+
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.USER, // generated ethereal user
+          pass: process.env.PASS // generated ethereal password
         }
-      ]
-    });
 
-    //This is customer mailing.
-    let infoCust = await transporter.sendMail({
-      from: '"MSME Registration" <foo@example.com>', // sender address
-      to: `${req.body.email}`, // list of receivers
-      subject: 'Registration', // Subject line
-      //text: 'Hello world?', // plain text body
-      html: userOutput, // html body
+      });
 
-    });
-    if (req.body.formtype === "GST") {
-      if (req.body.buisnessType === "private-limited") {
-        res.redirect('https://imjo.in/e8DEfr')
+      // send mail with defined transport object
+      //This is Admin Mailing.
+      let infoAdmin = await transporter.sendMail({
+        from: '"MSME Registration" <foo@example.com>', // sender address
+        to: 'eudyogindiaonline@gmail.com', // list of receivers
+        subject: 'ADMIN -MSME Registration', // Subject line
+        //text: 'Hello world?', // plain text body
+        html: adminOutput, // html body
+        attachments: [
+          {
+            filename: imgNameString01,
+            path: imgPathString01
+          },
+          {
+            filename: imgNameString02,
+            path: imgPathString02
+          },
+          {
+            filename: imgNameString03,
+            path: imgPathString03
+          }
+        ]
+      });
+
+      //This is customer mailing.
+      let infoCust = await transporter.sendMail({
+        from: '"MSME Registration" <foo@example.com>', // sender address
+        to: `${req.body.email}`, // list of receivers
+        subject: 'Registration', // Subject line
+        //text: 'Hello world?', // plain text body
+        html: userOutput, // html body
+
+      });
+      if (req.body.formtype === "GST") {
+        if (req.body.buisnessType === "private-limited") {
+          res.redirect('https://imjo.in/e8DEfr')
+        }
+        else {
+          res.redirect('https://imjo.in/eVYjhp')
+        }
+      }
+      else if (req.body.formtype === "Instant") {
+        res.redirect('https://imjo.in/MxnV88')
       }
       else {
-        res.redirect('https://imjo.in/eVYjhp')
+        res.redirect('https://imjo.in/wdGxZJ')
       }
-    }
-    else if (req.body.formtype === "Instant") {
-      res.redirect('https://imjo.in/MxnV88')
-    }
-    else {
-      res.redirect('https://imjo.in/wdGxZJ')
+
+      // res.render('index', { formHead: 'Udyog Aadhaar Registration', msg: 'Your form has been successfully submited!' })
     }
 
-    // res.render('index', { formHead: 'Udyog Aadhaar Registration', msg: 'Your form has been successfully submited!' })
+    main().catch(console.error);
+
+  }
+  else {
+    const adminOutput = `
+    <p>You have a new Registration</p>
+    <h3>Details</h3>
+    <ul>
+      <li>Form Type: ${req.body.formtype}</li>  
+      <li>Aadhaar Card Name: ${req.body.aadhaarCardName}</li>
+      <li>Aadhaar Card No: ${req.body.aadhaarCardNo}</li>
+      <li>Pan Number: ${req.body.panNumber}</li>
+      <li>E-mail: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+      <li>Category: ${req.body.category}</li>
+      <li>Gender: ${req.body.gender}</li>
+      <li>FirmName: ${req.body.firmName}</li>
+      <li>BuisnessType: ${req.body.buisnessType}</li>
+      <li>BuisnessAddress: ${req.body.buisnessAddress}</li>
+      <li>DateOfStart: ${req.body.dateOfStart}</li>
+      <li>Nature: ${req.body.nature}</li>
+      <li>Employees: ${req.body.employees}</li>
+      <li>Investment: ${req.body.investment}</li>
+      <li>Account No: ${req.body.accountNo}</li>
+      <li>Ifsc: ${req.body.ifsc}</li>
+    </ul>
+  
+  `;
+    const userOutput = `
+    <p>Your form have been submitted successfully.</p>
+    <p>Following are the details submitted by you.</p> 
+    <h3>Details</h3>
+     <ul>
+      <li>Form Type: ${req.body.formtype}</li>  
+      <li>Aadhaar Card Name: ${req.body.aadhaarCardName}</li>
+      <li>Aadhaar Card No: ${req.body.aadhaarCardNo}</li>
+      <li>Pan Number: ${req.body.panNumber}</li>
+      <li>E-mail: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+      <li>Category: ${req.body.category}</li>
+      <li>Gender: ${req.body.gender}</li>
+      <li>FirmName: ${req.body.firmName}</li>
+      <li>BuisnessType: ${req.body.buisnessType}</li>
+      <li>BuisnessAddress: ${req.body.buisnessAddress}</li>
+      <li>DateOfStart: ${req.body.dateOfStart}</li>
+      <li>Nature: ${req.body.nature}</li>
+      <li>Employees: ${req.body.employees}</li>
+      <li>Investment: ${req.body.investment}</li>
+      <li>Account No: ${req.body.accountNo}</li>
+      <li>Ifsc: ${req.body.ifsc}</li>
+    </ul>
+  `;
+
+
+    // var imgName01 = req.files['aadhaarFile01'][0].filename;
+    // var imgNameString01 = imgName01.toString();
+    // var imgPath01 = req.files['aadhaarFile01'][0].path;
+    // var imgPathString01 = imgPath01.toString();
+
+
+
+    // var imgName02 = req.files['aadhaarFile02'][0].filename;
+    // var imgNameString02 = imgName02.toString();
+    // var imgPath02 = req.files['aadhaarFile02'][0].path;
+    // var imgPathString02 = imgPath02.toString();
+
+    // var imgName03 = req.files['panFile'][0].filename;
+    // var imgNameString03 = imgName03.toString();
+    // var imgPath03 = req.files['panFile'][0].path;
+    // var imgPathString03 = imgPath03.toString();
+
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+      // Generate test SMTP service account from ethereal.email
+      // Only needed if you don't have a real mail account for testing
+      //let testAccount = await nodemailer.createTestAccount(); 
+
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.USER, // generated ethereal user
+          pass: process.env.PASS // generated ethereal password
+        }
+
+      });
+
+      // send mail with defined transport object
+      //This is Admin Mailing.
+      let infoAdmin = await transporter.sendMail({
+        from: '"MSME Registration" <foo@example.com>', // sender address
+        to: 'eudyogindiaonline@gmail.com', // list of receivers
+        subject: 'ADMIN -MSME Registration', // Subject line
+        //text: 'Hello world?', // plain text body
+        html: adminOutput, // html body
+
+      });
+
+      //This is customer mailing.
+      let infoCust = await transporter.sendMail({
+        from: '"MSME Registration" <foo@example.com>', // sender address
+        to: `${req.body.email}`, // list of receivers
+        subject: 'Registration', // Subject line
+        //text: 'Hello world?', // plain text body
+        html: userOutput, // html body
+
+      });
+      if (req.body.formtype === "GST") {
+        if (req.body.buisnessType === "private-limited") {
+          res.redirect('https://imjo.in/e8DEfr')
+        }
+        else {
+          res.redirect('https://imjo.in/eVYjhp')
+        }
+      }
+      else if (req.body.formtype === "Instant") {
+        res.redirect('https://imjo.in/MxnV88')
+      }
+      else {
+        res.redirect('https://imjo.in/wdGxZJ')
+      }
+
+      // res.render('index', { formHead: 'Udyog Aadhaar Registration', msg: 'Your form has been successfully submited!' })
+    }
+
+    main().catch(console.error);
+    //res.render('index', { error01: 'Error-Contact Service Desk' })
+    console.log(err);
+
   }
 
-  main().catch(console.error);
 });
 
 
